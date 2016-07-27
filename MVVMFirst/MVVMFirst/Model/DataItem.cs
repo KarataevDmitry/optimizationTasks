480e766272d47b572797b04c63964e02c4e45567
LonelySoul <dkarataev1990@gmail.com>
2016-07-27 18:05:09 +0600

    Первая команда

---
+++ MVVMFirst/MVVMFirst/Model/DataItem.cs
@@ -0,0 +1,16 @@
+﻿namespace MVVMFirst.Model
+{
+    public class DataItem
+    {
+        public string Title
+        {
+            get;
+            private set;
+        }
+
+        public DataItem ( string title )
+        {
+            Title = title;
+        }
+    }
+}
---
+++ MVVMFirst/MVVMFirst/ViewModel/MainViewModel.cs
@@ -0,0 +1,73 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using MVVMFirst.Model;
+
+namespace MVVMFirst.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that the main View can data bind to.
+    /// <para>
+    /// See http://www.mvvmlight.net
+    /// </para>
+    /// </summary>
+    public class MainViewModel : ViewModelBase
+    {
+        private readonly IDataService _dataService;
+        private RelayCommand _command; 
+        public RelayCommand MyCommand { get; private set; }
+
+        /// <summary>
+        /// The <see cref="WelcomeTitle" /> property's name.
+        /// </summary>
+        public const string WelcomeTitlePropertyName = "WelcomeTitle";
+
+        private string _welcomeTitle = string.Empty;
+
+        /// <summary>
+        /// Gets the WelcomeTitle property.
+        /// Changes to that property's value raise the PropertyChanged event. 
+        /// </summary>
+        public string WelcomeTitle
+        {
+            get
+            {
+                return _welcomeTitle;
+            }
+            set
+            {
+                Set(ref _welcomeTitle, value);
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the MainViewModel class.
+        /// </summary>
+        public MainViewModel ( IDataService dataService )
+        {
+            _dataService = dataService;
+            _command = new RelayCommand(MyCommandExec);
+            MyCommand = _command;
+            _dataService.GetData(
+                ( item, error ) =>
+                {
+                    if ( error != null )
+                    {
+                        // Report error here
+                        return;
+                    }
+
+                    WelcomeTitle = item.Title;
+                });
+        }
+        private void MyCommandExec()
+        {
+            WelcomeTitle = "This is a MyCommandExec";
+        }
+        ////public override void Cleanup()
+        ////{
+        ////    // Clean up if needed
+
+        ////    base.Cleanup();
+        ////}
+    }
+}
