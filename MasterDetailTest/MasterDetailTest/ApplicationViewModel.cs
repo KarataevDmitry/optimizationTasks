9c44376f6f4dc9d40ff294b5a51b44f275057ec2
Каратаев Дмитрий <dkarataev@syntellect.ru>
2020-07-08 03:59:08 +0600

    master-detail test case

---
+++ MasterDetailTest/MasterDetailTest/ApplicationViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace MasterDetailTest
+{
+    public class ApplicationViewModel : INotifyPropertyChanged
+    {
+        private Phone selectedPhone;
+
+        public ObservableCollection<Phone> Phones { get; set; }
+        public Phone SelectedPhone
+        {
+            get => selectedPhone;
+            set
+            {
+                selectedPhone = value;
+                OnPropertyChanged(nameof(SelectedPhone));
+            }
+        }
+
+        public ApplicationViewModel()
+        {
+            List<Person> iphOwners = new List<Person>()
+            {
+                new Person(){Name = "Иван", Surname = "Иванов"},
+                new Person(){Name = "Иван", Surname = "Петров"}
+            };
+            List<Person> samsOwners = new List<Person>()
+            {
+                new Person(){Name = "Иван", Surname = "Samsung"},
+                new Person(){Name = "Иван", Surname = "SamPetr"}
+            };
+            List<Person> hpOwns = new List<Person>()
+            {
+                new Person(){Name = "Иван", Surname = "HPIV"},
+                new Person(){Name = "Иван", Surname = "HPPetr"}
+            };
+            List<Person> xiOwns = new List<Person>()
+            {
+                new Person(){Name = "Иван", Surname = "XiIv"},
+                new Person(){Name = "Иван", Surname = "XiPetr"}
+            };
+            Phones = new ObservableCollection<Phone>
+            {
+                new Phone { Title="iPhone 7", Manufacturer= "Apple", Price=56000, Owners = new ObservableCollection<Person>(iphOwners)},
+                new Phone {Title="Galaxy S7 Edge", Manufacturer="Samsung", Price =60000, Owners = new ObservableCollection<Person>(samsOwners) },
+                new Phone {Title="Elite x3", Manufacturer="HP", Price=56000, Owners =  new ObservableCollection<Person>(hpOwns)},
+                new Phone {Title="Mi5S", Manufacturer="Xiaomi", Price=35000, Owners = new ObservableCollection<Person>(xiOwns)}
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ MasterDetailTest/MasterDetailTest/Phone.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using MasterDetailTest.Annotations;
+
+namespace MasterDetailTest
+{
+    public class Phone: INotifyPropertyChanged
+    {
+        private string title;
+        private int price;
+        private string manufacturer;
+        private ObservableCollection<Person> owners;
+        public string Title {
+            get => this.title;
+            set
+            {
+                this.title = value;
+                OnPropertyChanged(nameof(Title));
+            }
+        }
+        public  int Price
+        {
+            get => this.price;
+
+            set
+            {
+                this.price = value;
+                OnPropertyChanged(nameof(Price));
+            }
+        }
+
+        public ObservableCollection<Person> Owners
+        {
+            get => this.owners;
+            set
+            {
+                this.owners = value;
+                OnPropertyChanged(nameof(Owners));
+            }
+        }
+
+        public string Manufacturer
+        {
+            get => this.manufacturer;
+            set
+            {
+                this.manufacturer = value;
+                OnPropertyChanged(nameof(Manufacturer));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class Person:INotifyPropertyChanged
+    {
+        private string name;
+        private string surname;
+        public string Surname
+        {
+            get => this.surname;
+            set
+            {
+                this.surname = value;
+                OnPropertyChanged(nameof(Surname));
+            }
+        }
+        public string Name
+        {
+            get => this.name;
+            set
+            {
+                this.name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
